0329accf37590c86a7ab6da20506eab1d9e93d43
Aya <ayahame99@gmail.com>
2026-09-21 19:57:56 +0300

    provider and shared pref to save mode of application

--- lib/core/provider/settings.dart
+++ lib/core/provider/settings.dart
@@ -3,11 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingServicesProvider with ChangeNotifier {
   ThemeMode currentMode = ThemeMode.dark;
-
-
-
   static const String _themeKey = 'theme_mode';
-
 
   SettingServicesProvider() {
     _loadSettings();
@@ -16,8 +12,6 @@ class SettingServicesProvider with ChangeNotifier {
 
   Future<void> _loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
-
-
     final isDark = prefs.getBool(_themeKey);
     if (isDark != null) {
       currentMode = isDark ? ThemeMode.dark : ThemeMode.light;
---
+++ lib/core/utils/validator.dart
@@ -0,0 +1,48 @@
+class Validator {
+  static String? validateEmail(String? val) {
+    final RegExp emailRegex = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+    );
+    if (val == null) {
+      return 'this field is required';
+    } else if (val.trim().isEmpty) {
+      return 'this field is required';
+    } else if (emailRegex.hasMatch(val) == false) {
+      return 'Enter a valid E-mail';
+    } else {
+      return null;
+    }
+  }
+
+static String? validatePassword(String? val) {
+  if (val == null || val.trim().isEmpty) {
+    return 'This field is required';
+  }
+
+  if (val.length < 12) {
+    return 'Password must be at least 12 characters';
+  }
+
+  if (!RegExp(r'[A-Z]').hasMatch(val)) {
+    return 'Password must contain at least one uppercase letter';
+  }
+
+  if (!RegExp(r'[a-z]').hasMatch(val)) {
+    return 'Password must contain at least one lowercase letter';
+  }
+
+  if (!RegExp(r'[0-9]').hasMatch(val)) {
+    return 'Password must contain at least one digit';
+  }
+
+  if (!RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(val)) {
+    return 'Password must contain at least one special character';
+  }
+
+  return null;
+}
+
+
+
+
+}
