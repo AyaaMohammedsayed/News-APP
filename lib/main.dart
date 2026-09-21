292b257d25fcf58f62cd46709f756bc8d72893dc
Aya <ayahame99@gmail.com>
2026-09-21 17:33:47 +0300

    adding splash screen

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news/features/home/view/screens/home_screen.dart';
+
+void main() {
+  runApp(NewsApp());
+}
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          HomeScreen.routeName:(_)=>HomeScreen()
+        },
+        initialRoute: HomeScreen.routeName,
+      ),
+    );
+  }
+}
