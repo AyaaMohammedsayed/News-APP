0385fd9f709e31110821e4084436412aa955d4ff
Aya <ayahame99@gmail.com>
2026-09-21 19:57:23 +0300

    handeling  states  of response API

---
+++ lib/core/utils/loading_indecator.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoadingIndecator extends StatelessWidget {
+  const LoadingIndecator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(color: Colors.white,),
+    );
+  }
+}
